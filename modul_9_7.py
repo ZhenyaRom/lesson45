2bf63f82586b9f611ed8973c6dff74743a47db08
ZhenyaRom <evladro2020@gmail.com>
2024-08-21 16:51:48 +0500

    Add files via upload

---
+++ modul_9_7.py
@@ -0,0 +1,18 @@
+def is_prime(func):
+    def wrapper(*args, **kwargs):
+        summa = func(*args, **kwargs)
+        if summa < 2:
+            return summa
+        elif summa > 2:
+            for i in range(2, abs(summa) - 1):
+                if summa % i == 0:
+                    return f'Составное\n{summa}'
+        return f'Простое\n{summa}'
+    return wrapper
+
+@is_prime
+def sum_three(one, two, three):
+    return one + two + three
+
+result = sum_three(2, 3, 6)
+print(result)
